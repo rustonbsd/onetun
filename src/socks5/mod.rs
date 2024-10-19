29d2d375b36396162d168d7912e1e278254beb09
rustonbsd <rustonbsd@mailfence.com>
2024-10-20 01:46:55 +0200

    sync

--- src/socks5/mod.rs
+++ src/socks5/mod.rs
@@ -23,7 +23,7 @@ use tokio::{
 
 use base64::prelude::*;
 
-async fn start_server() -> Result<(), IoError> {
+pub async fn start_server() -> Result<(), IoError> {
     let listener = TcpListener::bind("127.0.0.1:5000").await?;
     let auth = Arc::new(NoAuth) as Arc<_>;
 
